6f6f887e302745bcf40b73c06d3b942783d18567
Steve Stevens <steve.h.i.stevens@gmail.com>
2019-05-19 12:58:51 +0100

    x-cinn launcher updater tweak

--- roles/scripts/add_x-cinnamon_launcher.sh
+++ roles/scripts/add_x-cinnamon_launcher.sh
@@ -4,10 +4,10 @@ application="${1}.desktop"
 home="/home/${USER}"
 favourites_source="/usr/share/applications"
 favourites_dest="${home}/.cinnamon/panel-launchers"
-favourites_config="${home}/.cinnamon/configs/panel-launchers@cinnamon.org/3.json"
+favourites_config="${home}/.cinnamon/configs/grouped-window-list@cinnamon.org/3.json"
 
 already_exists() {
-  test=$(jq '.launcherList.value[] | select( contains("'${application}'") ) ' ${favourites_config} )
+  test=$(jq '.["pinned-apps"].value[] | select( contains("'${application}'") ) ' ${favourites_config} )
   if [[ -n "${test}" ]]
     then echo true; else echo false; fi
 }
@@ -17,7 +17,7 @@ if ! $(already_exists) ; then
   ln -sf "${favourites_source}/${application}" "${favourites_dest}/${application}"
 
   # Add the desktop file name to the configuration file
-  jq --indent 4 '.launcherList.value[.launcherList.value| length] |= .+ "'${application}'"' "${favourites_config}" > "${favourites_config}_tmp" && mv "${favourites_config}_tmp" "${favourites_config}"
+  jq --indent 4 '.["pinned-apps"].value[.["pinned-apps"].value| length] |= .+ "'${application}'"' "${favourites_config}" > "${favourites_config}_tmp" && mv "${favourites_config}_tmp" "${favourites_config}"
 fi
 
 
